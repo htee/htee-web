bba51c1681a7b163785b75583359f748d6650362
Ben Burkert <ben@benburkert.com>
2014-07-08 11:19:42 -0700

    Fix the specs.
    :login was switched to :owner.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe ApplicationController, :type => :controller do
 
     describe "without a name" do
       it "returns a 204 & Location header to the stream" do
-        post :record, login: user.login
+        post :record, owner: user.login
 
         expect(response).to be_success
         expect(response.status).to eq(204)
@@ -28,8 +28,8 @@ RSpec.describe ApplicationController, :type => :controller do
         expect(location).to match(%r{^/#{user.login}/[0-9a-f]{20}$})
       end
 
-      it "returns 403 when the login does not exist" do
-        post :record, login: "fake"
+      it "returns 403 when the owner does not exist" do
+        post :record, owner: "fake"
 
         expect(response).to_not be_success
         expect(response.status).to eq(401)
@@ -38,7 +38,7 @@ RSpec.describe ApplicationController, :type => :controller do
 
     describe "with the name of a new stream" do
       it "returns a 204 & Location header to the stream" do
-        post :record, login: user.login, name: 'test-stream'
+        post :record, owner: user.login, name: 'test-stream'
 
         expect(response).to be_success
         expect(response.status).to eq(204)
@@ -48,7 +48,7 @@ RSpec.describe ApplicationController, :type => :controller do
       end
 
       it "converts a url unsafe name" do
-        post :record, login: user.login, name: 'hello world'
+        post :record, owner: user.login, name: 'hello world'
 
         expect(response).to be_success
         expect(response.status).to eq(204)
@@ -63,14 +63,14 @@ RSpec.describe ApplicationController, :type => :controller do
     it "returns 404 if the user does not exist" do
       stream = user.streams.create
 
-      get :playback, login: 'fake', name: stream.name
+      get :playback, owner: 'fake', name: stream.name
 
       expect(response).to_not be_success
       expect(response.status).to eq(404)
     end
 
     it "returns 404 if the stream does not exist" do
-      get :playback, login: user.login, name: 'fake-stream'
+      get :playback, owner: user.login, name: 'fake-stream'
 
       expect(response).to_not be_success
       expect(response.status).to eq(404)
@@ -79,7 +79,7 @@ RSpec.describe ApplicationController, :type => :controller do
     it "returns a 204 for a stream" do
       stream = user.streams.create
 
-      get :playback, login: user.login, name: stream.name
+      get :playback, owner: user.login, name: stream.name
 
       expect(response).to be_success
       expect(response.status).to eq(204)
